6e49072bb355a21bd75d55b23f9ee5e86f33d623
Sanja Hrenovica <shrenovica1@etf.unsa.ba>
2017-05-14 19:17:46 +0200

    Dodane forme

--- Projekat/ProjekatZatvor/MainPage.xaml.cs
+++ Projekat/ProjekatZatvor/MainPage.xaml.cs
@@ -25,6 +25,69 @@ namespace ProjekatZatvor
         public MainPage()
         {
             this.InitializeComponent();
+
+        }
+        public class Params {
+            public int myProperty { get; set; }
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            
+        }
+        private void PrikaziMeni_Click(object sender, RoutedEventArgs e) {
+            MojSplitView.IsPaneOpen = !MojSplitView.IsPaneOpen;
+        }
+        
+       /* public void PrikaziRaspored_Click(object sender, RoutedEventArgs e)
+        {
+           // var menuPodstranica = (e.AddedItems[0]);
+            sadrzajPodstranice.Navigate(typeof(UpravnikRaspored), new Params() { myProperty=42});
+            //sadrzajPodstranice.Navigate(typeof(MainPage1));
+
+        }*/
+        public void PrikaziZahtjeve_Click(object sender, RoutedEventArgs e)
+        {
+            // var menuPodstranica = (e.AddedItems[0]);
+            sadrzajPodstranice.Navigate(typeof(ZahtjeviUposlenika), new Params() { myProperty = 42 });
+            //sadrzajPodstranice.Navigate(typeof(MainPage1));
+
+        }
+        public void PrikaziNarudzbe_Click(object sender, RoutedEventArgs e)
+        {
+            // var menuPodstranica = (e.AddedItems[0]);
+            sadrzajPodstranice.Navigate(typeof(NarudzbeUpravnik), new Params() { myProperty = 42 });
+            //sadrzajPodstranice.Navigate(typeof(MainPage1));
+
+        }
+        public void PrikaziFinansije_Click(object sender, RoutedEventArgs e)
+        {
+            // var menuPodstranica = (e.AddedItems[0]);
+            sadrzajPodstranice.Navigate(typeof(Finansije), new Params() { myProperty = 42 });
+            //sadrzajPodstranice.Navigate(typeof(MainPage1));
+
+        }
+        public void PrikaziIzvjestaj_Click(object sender, RoutedEventArgs e)
+        {
+            // var menuPodstranica = (e.AddedItems[0]);
+            sadrzajPodstranice.Navigate(typeof(strazarIzvjestaj), new Params() { myProperty = 42 });
+            //sadrzajPodstranice.Navigate(typeof(MainPage1));
+
+        }
+        public void PrikaziZahtjev_Click(object sender, RoutedEventArgs e)
+        {
+            // var menuPodstranica = (e.AddedItems[0]);
+            sadrzajPodstranice.Navigate(typeof(PodnesiZahtjev), new Params() { myProperty = 42 });
+            //sadrzajPodstranice.Navigate(typeof(MainPage1));
+
+        }
+        public void PrikaziRaspored_Click(object sender, RoutedEventArgs e)
+        {
+            // var menuPodstranica = (e.AddedItems[0]);
+            sadrzajPodstranice.Navigate(typeof(strazarRaspored), new Params() { myProperty = 42 });
+            //sadrzajPodstranice.Navigate(typeof(MainPage1));
+
         }
     }
 }
